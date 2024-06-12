93b6feb0a6a06ccceb0cf6d5ca2a6269f9fcb156
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-06-12 22:07:13 +0800

    "resolve sync issue git"

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import os
 
 app = Flask(__name__)
 
+
 # Fungsi untuk membuat direktori jika belum ada
 def ensure_uploads_dir():
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+
 
 def perbesar(berkas, sy, sx):
     F = np.array(berkas)
@@ -26,39 +28,50 @@ def perbesar(berkas, sy, sx):
 
     return F2
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
     ensure_uploads_dir()
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"})
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"})
     if file:
-        sy = float(request.form['sy'])
-        sx = float(request.form['sx'])
-        filepath = os.path.join('uploads', file.filename)
+        sy = float(request.form["sy"])
+        sx = float(request.form["sx"])
+        filepath = os.path.join("uploads", file.filename)
         file.save(filepath)
-        image = Image.open(filepath).convert('L')  # Mengubah gambar menjadi grayscale untuk proses
+        image = Image.open(filepath).convert(
+            "L"
+        )  # Mengubah gambar menjadi grayscale untuk proses
         image_array = np.array(image)
-        
+
         # Terapkan fungsi perbesar
         scaled_image_array = perbesar(image_array, sy, sx)
-        
+
         # Konversi kembali ke gambar untuk ditampilkan atau disimpan
         scaled_image = Image.fromarray(scaled_image_array)
-        processed_filepath = os.path.join('uploads', 'processed_' + file.filename)
+        processed_filepath = os.path.join("uploads", "processed_" + file.filename)
         scaled_image.save(processed_filepath)
-        
-        return jsonify({'message': 'Image uploaded and scaled successfully', 'filepath': processed_filepath})
 
-@app.route('/uploads/<filename>')
+        return jsonify(
+            {
+                "message": "Image uploaded and scaled successfully",
+                "filepath": processed_filepath,
+            }
+        )
+
+
+@app.route("/uploads/<filename>")
 def uploaded_file(filename):
-    return send_from_directory('uploads', filename)
+    return send_from_directory("uploads", filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
